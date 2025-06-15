0138a5fbc7bd1a6774bc7a4b1b5d7b3b75ed0805
Fernando VI. <f3rnandogaff@gmail.com>
2025-06-15 14:16:45 -0600

    Deployment del proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,12 @@ const sass = require('gulp-sass')(require('sass'));
 //Compilar imágenes
 const imagemin = require('gulp-imagemin');
 
+//Purge en css
+const purgecss = require('gulp-purgecss');
+
+//rename
+const rename = require('gulp-rename');
+
 //Para compilar sass es necesario seguir 3 pasos:
 
 function css(done){
@@ -16,11 +22,28 @@ function css(done){
     //2- Compilar SASS
         .pipe(sass().on('error', sass.logError))
     //3- Exportalo o guardalo en una ubicación
-        .pipe(dest('build/css'))     
-        
+        .pipe(dest('build/css'))             
     //Callback
     done();
 }
+
+//Función para eliminar el código css que no se utiliza
+function cssbuild (done){
+    src('build/css/app.css')
+        .pipe(rename ({
+            //El nombre que se le añadirá al archivo
+            suffix: '.min'
+        }))
+        .pipe(purgecss ({
+            //Configuración de purge css
+            content: ['index.html']
+        }))
+        .pipe(dest ('build/css'))
+
+    done();
+}
+
+
 
 //Agregar un watch (permite observar por cambios en ciertos archivos)
 function dev( ){
@@ -44,3 +67,4 @@ exports.dev = dev;
 exports.css = css;
 exports.imagenes = imagenes;
 exports.default = series(imagenes, css, dev);
+exports.build = series(cssbuild);
